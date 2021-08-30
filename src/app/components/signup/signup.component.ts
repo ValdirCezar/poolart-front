39557d76c5846bb9f71ec58618b45e7014432b5c
Valdir Cezar <valdircezar312@gmail.com>
2021-08-29 21:33:19 -0300

    validFields method

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
-import { cpf } from 'cpf-cnpj-validator'; 
+import { cpf } from 'cpf-cnpj-validator';
 
 @Component({
   selector: 'app-signup',
@@ -14,51 +14,36 @@ export class SignupComponent implements OnInit {
 
   constructor(private tostr: ToastrService) { }
 
-  name:     FormControl = new FormControl(null, Validators.required)
-  cpf:      FormControl = new FormControl(null, Validators.required)
-  phone:    FormControl = new FormControl(null, Validators.required)
-  mail:     FormControl = new FormControl(null, Validators.email)
+  name: FormControl = new FormControl(null, Validators.required)
+  cpf: FormControl = new FormControl(null, Validators.required)
+  phone: FormControl = new FormControl(null, Validators.required)
+  mail: FormControl = new FormControl(null, Validators.email)
   password: FormControl = new FormControl(null, [Validators.minLength(5), Validators.maxLength(50)])
 
-  ngOnInit(): void {}
-  
-  validFields(): void {
-    this.validName();
-    this.validCpf();
-    this.validEmail();
-    this.validPhone();
-    this.validPassword();
-    this.cpfValidator();
-  }
-  
-  cpfValidator(): void {
-    if(!cpf.isValid(this.test)) 
+  ngOnInit(): void { }
+
+  validFields(): boolean {
+    if (!cpf.isValid(this.test)) {
       this.tostr.warning("Digite um CPF válido", "CPF")
-  }
-  
-  validName(): void {
-    if(this.name.invalid)
-    this.tostr.warning("O campo NOME deve ter entre 3 e 50 caracteres", "Nome")
-  }
-
-  validCpf(): void {
-    if(this.cpf.invalid)
-    this.tostr.warning("O campo CPF deve ser preenchido", "CPF")
-  }
-
-  validPhone(): void {
-    if(this.phone.invalid)
-    this.tostr.warning("O campo TELEFONE deve ser preenchido", "TELEFONE")
-  }
-
-  validEmail(): void {
-    if(this.mail.invalid)
-    this.tostr.warning("O campo E-MAIL deve ser preenchido", "E-MAIL")
+      return false
+    } else if (this.name.invalid) {
+      this.tostr.warning("O campo NOME deve ter entre 3 e 50 caracteres", "Nome")
+      return false
+    } else if (this.cpf.invalid) {
+      this.tostr.warning("O campo CPF deve ser preenchido", "CPF")
+      return false
+    } else if (this.phone.invalid) {
+      this.tostr.warning("O campo TELEFONE deve ser preenchido", "TELEFONE")
+      return false
+    } else if (this.mail.invalid) {
+      this.tostr.warning("O campo E-MAIL deve ser preenchido", "E-MAIL")
+      return false
+    } else if (this.password.invalid) {
+      this.tostr.warning("O campo SENHA deve entre 6 e 50 caracteres", "SENHA")
+      return false
+    }
+    return true
   }
 
-  validPassword(): void {
-    if(this.password.invalid)
-    this.tostr.warning("O campo SENHA deve entre 6 e 50 caracteres", "SENHA")
-  }
 
 }
