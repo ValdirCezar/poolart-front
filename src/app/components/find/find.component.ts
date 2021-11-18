293e39de4d765e35508fe96b9130e9a046bdb073
Valdir Cezar <valdircezar312@gmail.com>
2021-11-18 20:35:44 -0300

    finish rating

--- src/app/components/find/find.component.ts
+++ src/app/components/find/find.component.ts
@@ -44,4 +44,10 @@ export class FindComponent implements AfterViewInit, OnInit {
       console.error(err);
     })
   }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  
 }
--- src/app/components/rating/rating.component.ts
+++ src/app/components/rating/rating.component.ts
@@ -1,34 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from '../../services/user.service';
-import { ToastrService } from 'ngx-toastr';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "../../services/user.service";
+import { ToastrService } from "ngx-toastr";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-rating',
-  templateUrl: './rating.component.html',
-  styleUrls: ['./rating.component.css']
+  selector: "app-rating",
+  templateUrl: "./rating.component.html",
+  styleUrls: ["./rating.component.css"],
 })
 export class RatingComponent implements OnInit {
 
-  rating: number = 4;
+  stars = 0;
+  userId: string = "0";
 
   constructor(
     private userService: UserService,
     private toastService: ToastrService,
+    private route: ActivatedRoute,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit(): void {
+    this.userId = this.route.snapshot.paramMap.get("id");
   }
 
   sendRating(): void {
-    this.userService.sendRating(1, this.rating).subscribe(() => {
-      this.toastService.success("Avaliação realizada com sucesso!");
-      this.router.navigate(['../../dashboard/find'])
-    }, () => {
-      this.toastService.success("Falha na avaliação :( Tente posteriormente");
-      this.router.navigate(['../../find'])
-    })
-  }
+    this.userService.sendRating(this.userId, this.stars).subscribe(
+      () => {
+        console.log(this.stars);
 
+        this.toastService.success("Avaliação realizada com sucesso!", "Avaliação", {
+          positionClass: "toast-top-right"
+        });
+        this.router.navigate(["../../dashboard/find"]);
+      },
+      () => {
+        this.toastService.success("Falha na avaliação :( Tente posteriormente");
+        this.router.navigate(["../../find"]);
+      }
+    );
+  }
 }
