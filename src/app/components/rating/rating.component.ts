5470f71f69d0aaaab56fc01b28d24e8b686eec1f
Valdir Cezar <valdircezar312@gmail.com>
2021-11-18 19:42:33 -0300

    adjusts route to rating

--- src/app/components/rating/rating.component.ts
+++ src/app/components/rating/rating.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-rating',
@@ -11,16 +13,21 @@ export class RatingComponent implements OnInit {
   rating: number = 4;
 
   constructor(
-    private userService: UserService
+    private userService: UserService,
+    private toastService: ToastrService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
   }
 
   sendRating(): void {
-    this.userService.sendRating(1, this.rating).subscribe(res => {
-      console.log(res);
-      
+    this.userService.sendRating(1, this.rating).subscribe(() => {
+      this.toastService.success("Avaliação realizada com sucesso!");
+      this.router.navigate(['../../dashboard/find'])
+    }, () => {
+      this.toastService.success("Falha na avaliação :( Tente posteriormente");
+      this.router.navigate(['../../find'])
     })
   }
 
